bc65947f05ea3a93178da5f03e976c4013874f04
samason4 <102568722+samason4@users.noreply.github.com>
2023-06-20 13:40:38 -0500

    added Munit and MBH
    Munit is used for code unit to cgs conversion, and I wasn't sure what an appropriate value is so I just set it to one for now. MBH is the mass of the black hole in grams (for now I set it to 8.26e39g, which is the mass of sag A*)

--- core/decs.h
+++ core/decs.h
@@ -299,6 +299,8 @@ extern int nthreads;
 #define KTOTMAX (3.)
 #define ME (9.1093826e-28  ) // Electron mass
 #define MP (1.67262171e-24 ) // Proton mass
+#define MBH (8.26e39) // Mass of the black hole
+#defien Munit (1) // Munit for code<->CGS conversion
 extern double game, gamp;
 extern double fel0;
 extern double tptemin, tptemax;
