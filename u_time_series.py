c16f96cfad607953c28b4957db88c44de8025e80
samason4 <102568722+samason4@users.noreply.github.com>
2023-06-21 15:14:56 -0500

    put stuff back in function

--- u_time_series.py
+++ u_time_series.py
@@ -20,30 +20,30 @@ def run_parallel(function, dlist,	nthreads):
 	pool.close()
 	pool.join()
 
-#def plotting(dumpno):	
+def plotting(dumpno):	
 	
-# header info
-header = open(os.path.join(dumpsdir,'dump_0000{0:04d}'.format(dumpno)),'r')
-firstline = header.readline()
-header.close()
-firstline = firstline.split()
+	# header info
+	header = open(os.path.join(dumpsdir,'dump_0000{0:04d}'.format(dumpno)),'r')
+	firstline = header.readline()
+	header.close()
+	firstline = firstline.split()
 
-madtype = int(firstline[0])
-rin = float(firstline[2])	
-rmax = float(firstline[3])
-n1 = int(firstline[11])
-n2 = int(firstline[12])
+	madtype = int(firstline[0])
+	rin = float(firstline[2])	
+	rmax = float(firstline[3])
+	n1 = int(firstline[11])
+	n2 = int(firstline[12])
 
-t = '{:.3f}'.format(t)
+	t = '{:.3f}'.format(t)
 	
-# loading prims
-prims = np.loadtxt(os.path.join(dumpsdir,'dump_0000{0:04d}'.format(dumpno)),skiprows=1)
-u = prims[:,1].reshape((n1,n2))
-max = np.argmax(u, axis=0, out = none)
-index1 = 0
-for i in range(n2):
-	if(max > n1):
-		index1 += 1
-		max -= n1
-u_max = u[index1][max]
-print(umax)
+	# loading prims
+	prims = np.loadtxt(os.path.join(dumpsdir,'dump_0000{0:04d}'.format(dumpno)),skiprows=1)
+	u = prims[:,1].reshape((n1,n2))
+	max = np.argmax(u, axis=0, out = none)
+	index1 = 0
+	for i in range(n2):
+		if(max > n1):
+			index1 += 1
+			max -= n1
+	u_max = u[index1][max]
+	print(umax)
