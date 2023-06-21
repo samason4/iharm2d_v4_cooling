8ea73d63bedd797a5cc2f7eb7a37a757753f5a98
samason4 <102568722+samason4@users.noreply.github.com>
2023-06-20 20:10:37 -0500

    commented out cooling
    commented out the actual cooling update for testing purposes.

--- core/electrons.c
+++ core/electrons.c
@@ -116,7 +116,7 @@ inline void heat_electrons_1zone(struct GridGeom *G, struct FluidState *Ss, stru
     double m = 3.;
     double alpha1 = pow(r,-3/2)*m/ut;
     uel = uel*exp(-t/alpha1);
-    Sf->P[UU][j][i] = uel/(Munit*pow(Lunit,2)/pow(Tunit,2));
+    //Sf->P[UU][j][i] = uel/(Munit*pow(Lunit,2)/pow(Tunit,2));
   }
 
   // Reset total entropy
