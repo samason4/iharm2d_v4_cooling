ff63be81d6e635dd2965fd4267ee66479e29e5bb
samason4 <102568722+samason4@users.noreply.github.com>
2023-06-21 15:38:25 -0500

    lets see if we get the coords

--- u_time_series.py
+++ u_time_series.py
@@ -71,15 +71,16 @@ def plotting(dumpno):
 	# loading prims
 	prims = np.loadtxt(os.path.join(dumpsdir,'dump_0000{0:04d}'.format(dumpno)),skiprows=1)
 	u = prims[:,1].reshape((n1,n2))
-	max = np.argmax(u, axis=None)
+	maxarr = np.argmax(u, axis=None)
+	max = maxarr[0]
 	print(max)
-	#index1 = 0
-	#for i in range(n2):
-	#	if(max > n1):
-	#		index1 += 1
-	#		max -= n1
-	#u_max = u[index1][max]
-	#print(umax)
+	index1 = 0
+	for i in range(n2):
+		if(max > n1):
+			index1 += 1
+			max -= n1
+	u_max = u[index1][max]
+	print(umax)
 if __name__=="__main__":
 	dstart = int(sorted(glob.glob(os.path.join(dumpsdir,'dump*')))[0][-4:])
 	dend = int(sorted(glob.glob(os.path.join(dumpsdir,'dump*')))[-1][-4:])
