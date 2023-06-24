37dd8e1714e7f279e0bb65c636ba87fbfa1fe266
samason4 <102568722+samason4@users.noreply.github.com>
2023-06-24 12:01:24 -0500

    KEL->KEL0

--- core/cooling.c
+++ core/cooling.c
@@ -27,11 +27,11 @@ void cool_electrons_1zone(struct GridGeom *G, struct FluidState *S, int i, int j
   //dt is a global variable so we don't even need to initialize it
 
   //to fing uel:
-  double uel = pow(S->P[RHO][j][i],game)*exp(S->P[KEL][j][i]*(game-1));
+  double uel = pow(S->P[RHO][j][i],game)*exp(S->P[KEL0][j][i]*(game-1));
 
   //update the internal energy of the electrons at (i,j):
   uel -= uel/(m*pow(r, 1.5)*ut)*dt/2;
 
   //update the entropy with the new internal energy
-  S->P[KEL][j][i] = log(uel/pow(S->P[RHO][j][i], game))/(game-1);
+  S->P[KEL0][j][i] = log(uel/pow(S->P[RHO][j][i], game))/(game-1);
 }
