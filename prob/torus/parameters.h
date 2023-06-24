9bf1cf674c4e2a7d9d4639e0c807f375267c7125
samason4 <102568722+samason4@users.noreply.github.com>
2023-06-23 22:04:05 -0500

    turned off heating

--- prob/torus/parameters.h
+++ prob/torus/parameters.h
@@ -32,7 +32,7 @@
 // SUPPRESS_MAG_HEAT - (0,1) No electron heating when sigma > 1
 // BETA_HEAT         - (0,1) Beta-dependent electron heating
  
-#define ELECTRONS           1
+#define ELECTRONS           0 // temporarily turned off heating so I can test the cooling
 #define ALLMODELS           1
 #define SUPPRESS_HIGHB_HEAT 1
 #define BETA_HEAT           1
