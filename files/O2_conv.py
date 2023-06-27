185afba892ea8ce888cd483e7b79d9a54e20be6e
sammason4 <samason4@illinois.edu>
2023-06-27 14:48:55 -0500

    fixed double negative sign error

--- files/O2_conv.py
+++ files/O2_conv.py
@@ -338,7 +338,7 @@ sub1.loglog(resolutions, errors, 'bo')
 sub1.set_yscale('log')
 sub1.set_yticks([2**-22, 2**-21, 2**-20, 2**-19, 2**-18, 2**-17, 2**-16])
 def log_formatter(x, pos):
-    return f"2^-{int(-pos)}"
+    return f"2^{int(-pos)}"
 sub1.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(log_formatter))
 plt.ylabel("total error")
 plt.title("error vs resolution")
