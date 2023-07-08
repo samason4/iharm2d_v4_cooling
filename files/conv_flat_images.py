d404bc81a0d81d2ff430947d7d375c976256141e
sammason4 <samason4@illinois.edu>
2023-07-07 23:58:19 -0500

    added conv_flat_images.py

--- files/conv_flat_images.py
+++ files/conv_flat_images.py
@@ -7,7 +7,7 @@ from matplotlib import gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import multiprocessing as mp
 
-# python files/O2_conv_flat.py ./ ./dumps9 ./dumps5 ./dumps2 ./dumps05
+# python files/conv_flat_images.py ./ ./dumps9 ./dumps5 ./dumps2 ./dumps05
 
 # paths
 dumpsdir9 = sys.argv[2]
@@ -88,29 +88,14 @@ def analytical(dumpsdir, dumpno, prims):
 	u = u0*np.exp(alpha*float(t))
 	return([u, t])
 
-def find_error(dumpsdir, dump_min, dump_max):
-	error = 0
-	for i in range(dump_min, dump_max + 1):
-		min_r = 5
-		min_th = 5
-		prims = initial_prims(dumpsdir, min_r, min_th)
-		num = numerical(dumpsdir, i, min_r, min_th)[0]
-		ana = analytical(dumpsdir, i, prims)[0]
-		error += abs(num-ana)
-	error = error/((dump_max+1)-dump_min)
+def find_error(dumpsdir, dumpno):
+	min_r = 5
+	min_th = 5
+	prims = initial_prims(dumpsdir, min_r, min_th)
+	num = numerical(dumpsdir, i, min_r, min_th)[0]
+	ana = analytical(dumpsdir, i, prims)[0]
+	error = abs(num-ana)
 	return(error)
-
-#finding errors:
-cour_inv = []
-errors = []
-cour_inv.append(1/0.9)
-errors.append(find_error(dumpsdir9, 80, 100))
-cour_inv.append(1/0.5)
-errors.append(find_error(dumpsdir5, 80, 100))
-cour_inv.append(1/0.2)
-errors.append(find_error(dumpsdir2, 80, 100))
-cour_inv.append(1/0.05)
-errors.append(find_error(dumpsdir05, 80, 100))
 
 #this is for the comparison line:
 x = []
@@ -130,19 +115,31 @@ for i in range(12):
     res2.append(temp_res2)
     temp_res2 += 2
 
-#plotting:
-fig1, sub1 = plt.subplots()
-sub1.loglog(cour_inv, errors, color = 'b', label = 'Error of Test Cooling')
-sub1.loglog(res, x, color = 'r', label = 'Line of Slope N^-2 for Comparison')
-sub1.loglog(res2, x2, color = 'g', label = 'Line of Slope N^-1 for Comparison')
-sub1.loglog(cour_inv, errors, 'bo')
-plt.xticks([], [])
-sub1.set_xticks([])
-sub1.set_xticks([], minor=True)
-sub1.set_xticks([1, 2, 4, 8, 16, 32], ['2^0', '2^1', '2^2', '2^3', '2^4', '2^5'])
-plt.ylabel("Total Error")
-plt.xlabel("1 / The Courant Number")
-plt.title('Error vs 1/cour')
-plt.legend()
-plt.savefig('error_vs_cour.png')
-plt.close()
+#finding errors:
+cour_inv = []
+errors = []
+for i in range(201):
+    cour_inv.append(1/0.9)
+    errors.append(find_error(dumpsdir9, i))
+    cour_inv.append(1/0.5)
+    errors.append(find_error(dumpsdir5, i))
+    cour_inv.append(1/0.2)
+    errors.append(find_error(dumpsdir2, i))
+    cour_inv.append(1/0.05)
+    errors.append(find_error(dumpsdir05, i))
+    #plotting:
+    fig1, sub1 = plt.subplots()
+    sub1.loglog(cour_inv, errors, color = 'b', label = 'Error of Test Cooling')
+    sub1.loglog(res, x, color = 'r', label = 'Line of Slope N^-2 for Comparison')
+    sub1.loglog(res2, x2, color = 'g', label = 'Line of Slope N^-1 for Comparison')
+    sub1.loglog(cour_inv, errors, 'bo')
+    plt.xticks([], [])
+    sub1.set_xticks([])
+    sub1.set_xticks([], minor=True)
+    sub1.set_xticks([1, 2, 4, 8, 16, 32], ['2^0', '2^1', '2^2', '2^3', '2^4', '2^5'])
+    plt.ylabel("Total Error")
+    plt.xlabel("1 / The Courant Number")
+    plt.title('Error vs 1/cour')
+    plt.legend()
+    plt.savefig(os.path.join(outputdir9,'error_vs_cour_{0:04d}.png'.format(i)))
+    plt.close()
